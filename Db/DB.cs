b9b8bb3489c8f26621b9d5c661b6351e169e8659
chris <cpbuck12@hotmail.com>
2013-02-14 09:15:49 -0500

    New transactions coded, preliminary

--- Db/DB.cs
+++ Db/DB.cs
@@ -84,60 +84,105 @@ namespace Db
                 transaction.Commit();
             }
         }
+        public int AddDoctor(string firstName,string lastName,string shortName,string address1,string address2,string address3,string city,string locality1,string locality2,
+            string postalCode,string country,string voice,string fax,string email,string contact)
+        {
+            shortName = shortName.Trim().ToUpper();
+            if ((from dr0 in conciergeEntities_.doctors
+                 where dr0.shortname == shortName
+                 select dr0).Count() > 0)
+            {
+                return -1; // TODO: handle short name already extant
+            }
+            doctor dr = conciergeEntities_.doctors.CreateObject();
+            dr.firstname = firstName;
+            dr.lastname = lastName;
+            dr.shortname = shortName;
+            dr.address1 = address1;
+            dr.address2 = address2;
+            dr.address3 = address3;
+            dr.city = city;
+            dr.locality1 = locality1;
+            dr.locality2 = locality2;
+            dr.postal_code = postalCode;
+            dr.country = country;
+            dr.telephone = voice;
+            dr.fax = fax;
+            dr.email = email;
+            dr.contact_person = contact;
+            conciergeEntities_.doctors.AddObject(dr);
+            conciergeEntities_.SaveChanges();
+            return 0;
+        }
+        public int AddFiles(FileInfo[] files)
+        {
+            using (DbTransaction transaction = conciergeEntities_.Connection.BeginTransaction())
+            {
+                foreach (FileInfo file in files)
+                {
+                    int result = AddFile(file);
+                }
+                transaction.Commit();
+            }
+            return 0;
+        }
         public int AddFile(FileInfo file)
+        {
+            FileInfo[] files = new FileInfo[1];
+            files[0] = file;
+            return AddFiles(files);
+        }
+        int AddFile_(FileInfo file)
         {
             int id;
             string hash = FileHash(file);
 
-            using (DbTransaction transaction = conciergeEntities_.Connection.BeginTransaction())
+            document doc = conciergeEntities_.documents.CreateObject();
+            doc.path = file.FullName;
+            doc.checksum = "";
+            conciergeEntities_.documents.AddObject(doc);
+            conciergeEntities_.SaveChanges();
+            id = (from d in conciergeEntities_.documents
+                    select d.id).Max();
+            doc = (from d in conciergeEntities_.documents
+                    where d.id == id
+                    select d).First();
+            FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read);
+            SHA1Managed sha1 = new SHA1Managed();
+            int fullSegments = (int)(fileStream.Length / MaxDocumentSegmentSize);
+            int leftOver = (int)(fileStream.Length % MaxDocumentSegmentSize);
+            for (int iSegment = 0; iSegment < fullSegments; iSegment++)
             {
-                document doc = conciergeEntities_.documents.CreateObject();
-                doc.path = file.FullName;
-                doc.checksum = "";
-                conciergeEntities_.documents.AddObject(doc);
-                conciergeEntities_.SaveChanges();
-                id = (from d in conciergeEntities_.documents
-                      select d.id).Max();
-                doc = (from d in conciergeEntities_.documents
-                       where d.id == id
-                       select d).First();
-                FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read);
-                SHA1Managed sha1 = new SHA1Managed();
-                int fullSegments = (int)(fileStream.Length / MaxDocumentSegmentSize);
-                int leftOver = (int)(fileStream.Length % MaxDocumentSegmentSize);
-                for (int iSegment = 0; iSegment < fullSegments; iSegment++)
-                {
-                    byte[] buffer = new byte[MaxDocumentSegmentSize];
-                    int position = iSegment * MaxDocumentSegmentSize;
-                    fileStream.Read(buffer, 0, MaxDocumentSegmentSize);
-                    sha1.TransformBlock(buffer, 0, MaxDocumentSegmentSize, null, 0);
-                    document_segment segment = conciergeEntities_.document_segment.CreateObject();
-                    segment.document_id = id;
-                    segment.data = buffer;
-                    segment.position = iSegment;
-                    conciergeEntities_.document_segment.AddObject(segment);
-                }
-                if (leftOver > 0)
-                {
-                    byte[] buffer = new byte[leftOver];
-                    int position = (int)(fileStream.Length - leftOver);
-                    fileStream.Read(buffer, 0, leftOver);
-                    sha1.TransformFinalBlock(buffer, 0, leftOver);
-                    document_segment segment = conciergeEntities_.document_segment.CreateObject();
-                    segment.document_id = id;
-                    segment.data = buffer;
-                    segment.position = fullSegments;
-                    conciergeEntities_.document_segment.AddObject(segment);
-                }
-                else
-                    sha1.TransformFinalBlock(null, 0, 0);
-                string hashKey = string.Concat(from b in sha1.Hash
-                                               select b.ToString("X2"));
-                doc.checksum = hashKey;
-                doc.id = id;
-                conciergeEntities_.SaveChanges();
-                transaction.Commit();
+                byte[] buffer = new byte[MaxDocumentSegmentSize];
+                int position = iSegment * MaxDocumentSegmentSize;
+                fileStream.Read(buffer, 0, MaxDocumentSegmentSize);
+                sha1.TransformBlock(buffer, 0, MaxDocumentSegmentSize, null, 0);
+                document_segment segment = conciergeEntities_.document_segment.CreateObject();
+                segment.document_id = id;
+                segment.data = buffer;
+                segment.position = iSegment;
+                conciergeEntities_.document_segment.AddObject(segment);
             }
+            if (leftOver > 0)
+            {
+                byte[] buffer = new byte[leftOver];
+                int position = (int)(fileStream.Length - leftOver);
+                fileStream.Read(buffer, 0, leftOver);
+                sha1.TransformFinalBlock(buffer, 0, leftOver);
+                document_segment segment = conciergeEntities_.document_segment.CreateObject();
+                segment.document_id = id;
+                segment.data = buffer;
+                segment.position = fullSegments;
+                conciergeEntities_.document_segment.AddObject(segment);
+            }
+            else
+                sha1.TransformFinalBlock(null, 0, 0);
+            string hashKey = string.Concat(from b in sha1.Hash
+                                            select b.ToString("X2"));
+            doc.checksum = hashKey;
+            doc.id = id;
+            conciergeEntities_.SaveChanges();
+
             return id;
         }
         static public Db Instance()
